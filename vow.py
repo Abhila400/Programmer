aee4a612bfb225add984fdf1a1c9e271b32b02c7
Abhila400 <52188929+Abhila400@users.noreply.github.com>
2019-06-25 12:41:41 +0530

    Create vow.py

---
+++ vow.py
@@ -0,0 +1,8 @@
+a=input()
+if a.isalpha():
+    if a=='a'or a=='A'or a=='e'or a=='E'or a=='i'or a=='I'or a=='o'or a=='O'or a=='u'or a=='U':
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("Invalid")

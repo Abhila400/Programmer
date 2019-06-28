4e1fe3a82d8f8f9c0811a92617c3a877a7614e66
Abhila400 <52188929+Abhila400@users.noreply.github.com>
2019-06-28 23:14:04 +0530

    Create primeno.py

---
+++ primeno.py
@@ -0,0 +1,12 @@
+a=[int(i) for i in input().split()]
+for i in range (a[0]+1,a[1]):
+    flag=0
+    for j in range (2,i):
+        if i>2:
+            if i%j==0:
+                flag=1
+                break
+        else:
+            print(i)
+    if flag==0:
+        print(i)

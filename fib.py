91378ce1986355162661c427cebdf995f7ff4a8d
Abhila400 <52188929+Abhila400@users.noreply.github.com>
2019-07-06 11:15:34 +0530

    Update fib.py

--- fib.py
+++ fib.py
@@ -1,7 +1,10 @@
 n=int(input())
 a=[]
 a.append(1)
-a.append(1)
-for i in range (2,n):
-    a.append(a[i-1]+a[i-2])
-print(*a)
+if n==1:
+    print(*a)
+else:
+    a.append(1)
+    for i in range (2,n):
+        a.append(a[i-1]+a[i-2])
+    print(*a)

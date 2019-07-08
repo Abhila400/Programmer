8d05d2376f3db7d09b4d2a4da76691c96188a867
Abhila400 <52188929+Abhila400@users.noreply.github.com>
2019-07-08 21:40:46 +0530

    Update alphanum.py

--- alphanum.py
+++ alphanum.py
@@ -1,5 +1,16 @@
 string=input()
-if string.isalnum():
+n=len(string)
+digit=0
+alpha=0
+for i in range(0,n):
+    if string[i].isdigit():
+        digit=1
+        break
+for i in range(0,n):
+    if string[i].isalpha():
+        alpha=1
+        break
+if digit==1 and alpha==1:
     print("Yes")
 else:
     print("No")

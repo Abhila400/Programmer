9e212cb6c05c2d503fdbad7f4cdf9381b7f3fae2
Abhila400 <52188929+Abhila400@users.noreply.github.com>
2019-08-17 18:55:46 +0530

    Update cost.py

--- cost.py
+++ cost.py
@@ -2,13 +2,23 @@ m,s=input().split()
 m=list(m)
 s=list(s)
 c=0
-l=max(len(m),len(s))
-for i in range(0,l):
-    if i<len(m):
-        if m[i]==s[i]:
-            continue
+if len(m)<len(s):
+    for i in range(0,len(s)):
+        if i<len(m):
+            if m[i]==s[i]:
+                continue
+            else:
+                c+=1
         else:
             c+=1
-    else:
-        c+=1
+else:
+    for i in range(0,len(m)):
+        if len(m)>len(s):
+            m.pop()
+            c+=1
+        else:
+            break
+    for i in range(0,len(s)):
+        if m[i]!=s[i]:
+            c+=1
 print(c)
